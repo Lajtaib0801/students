107936d32b48d53527f8a0f576f2d10c5a419e85
Lajtaib0801 <lajtaibenja@gmail.com>
2023-12-08 13:56:43 +0100

    team model

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateStudentRequest;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
-    /**
+   /**
      * Display a listing of the resource.
      */
     public function index()
@@ -20,8 +22,35 @@ class StudentController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
+    // public function store(CreateStudentRequest $request)
     {
-        $student = Student::create($request->all());
+        $validator = Validator::make($request->all(), [
+            "first_name"=> "required|min:4",
+             "last_name"=> "required",
+             "email"=> "required|email|unique:studnets,email",
+             "rank"=> "integer",
+        ],[
+            "required" => "A(z) :attribute mező kitöltése kötelező!",
+            "email.unique"=> "Ez az e-mail cím már foglalt!"
+        ]);
+        $validated = $validator->validate();
+
+
+        // $request->validate([
+        //     "first_name"=> "required|min:4",
+        //     "last_name"=> "required",
+        //     "email"=> "required|email|unique:studnets,email",
+        //     "rank"=> "integer",
+        // ]);
+
+        // $student = Student::create($request->all());
+        $student = Student::create($validated);
+        // $student = Student::create([
+        //     "first_name"=> $request->first_name,
+        //     "last_name"=> $request->last_name,
+        //     "email"=> $request->email,
+        //     "rank"=> $request->rank,
+        // ]);
         return response()->json($student, 201);
     }
 
@@ -30,7 +59,12 @@ class StudentController extends Controller
      */
     public function show(string $id)
     {
-        return response()->json(Student::findOrFail($id));
+        // $student = Student::findOrFail($id);
+        $student = Student::find($id);
+        if ($student == null) {
+            return response()->json(["message" => "No student found."], 404);
+        }
+        return response()->json($student);
     }
 
     /**
@@ -38,9 +72,9 @@ class StudentController extends Controller
      */
     public function update(Request $request, string $id)
     {
-       $student = Student::find($id);
+        $student = Student::find($id);
         if ($student == null) {
-            return response()->json(["message"=> "No student found!"],404);
+            return response()->json(["message" => "No student found."], 404);
         }
         $student->update($request->all());
         return response()->json($student, 200);
@@ -53,9 +87,9 @@ class StudentController extends Controller
     {
         $student = Student::find($id);
         if ($student == null) {
-            return response()->json(["message"=> "No student found!"],404);
+            return response()->json(["message" => "No student found."], 404);
         }
         $student->delete();
-        return response(204);
+        return response(status: 204);
     }
 }
